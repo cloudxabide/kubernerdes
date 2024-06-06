99630b1fbef837b1c9a9b93f6f926a3badd5394d
James Radtke <cloudxabide@gmail.com>
2024-06-06 11:13:50 -0400

    generic update

--- Scripts/61_Ingress_Router.sh
+++ Scripts/61_Ingress_Router.sh
@@ -64,7 +64,8 @@ spec:
   ipAddressPools:
   - default
 EOF4
-# NOTE:  make sure that this runs successfully
+# NOTE:  make sure that this runs successfully - I have seen it fail at times (and I am not sure why)
+#  if it does, just rerun
 kubectl apply -f metallb-config.yaml
 cd -
 
